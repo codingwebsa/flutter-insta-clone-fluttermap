dd01cf4e1e9b36693601568ccd12c9e68505512c
CodingSA <codingwebsa@gmail.com>
2023-04-04 10:12:45 +0600

    Part 2 (Bottom nav)

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,91 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
+              icon: Icon(
+                Icons.home,
+                color: currentPage == 0
+                    ? Color.fromRGBO(203, 73, 101, 1)
+                    : Color.fromRGBO(40, 40, 40, 1),
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 1;
+                  });
+                },
+                icon: Icon(
+                  Icons.search,
+                  color: currentPage == 1
+                      ? Color.fromRGBO(203, 73, 101, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            const Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 2;
+                  });
+                },
+                icon: Icon(
+                  Icons.ondemand_video,
+                  color: currentPage == 2
+                      ? Color.fromRGBO(203, 73, 101, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            const Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 3;
+                  });
+                },
+                icon: Icon(
+                  Icons.card_travel,
+                  color: currentPage == 3
+                      ? Color.fromRGBO(203, 73, 101, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            const Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 4;
+                  });
+                },
+                icon: Icon(
+                  Icons.person,
+                  color: currentPage == 4
+                      ? Color.fromRGBO(203, 73, 101, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
   }
 }
